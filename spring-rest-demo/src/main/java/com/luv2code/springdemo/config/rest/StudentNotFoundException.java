6b347233781e60612c04578cf4fa7c186d6f1d48
Denis Kisina <kisinad@gmail.com>
2020-08-08 17:10:05 -0500

    Added generic error message

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/config/rest/StudentNotFoundException.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo.config.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+
+	}
+
+}
--- spring-rest-demo/src/main/java/com/luv2code/springdemo/config/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/config/rest/StudentRestController.java
@@ -3,7 +3,13 @@ package com.luv2code.springdemo.config.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +19,61 @@ import com.luv2code.springdemo.config.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	private List<Student> theStudents;
 
-		List<Student> theStudents = new ArrayList<>();
+//	define @Postconstruct to load student data once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 
 		theStudents.add(new Student("Denis", "Kisina"));
 		theStudents.add(new Student("Ben", "Obbo"));
 		theStudents.add(new Student("David", "Clark"));
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 
 		return theStudents;
 
+	}
+//	define end point for "/student/{studentId}" - return student at index
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		if ((studentId > theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student ID not found - " + studentId);
+		}
+
+		return theStudents.get(studentId);
+
+	}
+
+//	add exception handler 
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+//		create a student error response message
+		StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+
+		studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+		studentErrorResponse.setMessage(exc.getMessage());
+		studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(studentErrorResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+		StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+
+		studentErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+		studentErrorResponse.setMessage(exc.getMessage());
+		studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(studentErrorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 }
